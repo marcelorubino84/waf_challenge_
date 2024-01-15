972004080e9d0f815f6cbe08c1971581418440dd
marcelorubino84 <marcelo.rubino@icloud.com>
2024-01-15 17:39:19 +0100

    In this third commit, I initialized a counter called total_cost and set it to zero. Next, and if statement was created to check that there are more than 2 volumes. Inside the conditional a switch was created to treat every case of discount, i.e., 5% discount, 10%, etc.

--- book_store.c
+++ book_store.c
@@ -12,5 +12,30 @@ int main(void)
 float cost_calculation(int quant_volumes, int volumes[])
 {
     float total_cost = 0.0;
+
+    if (quant_volumes > 2)
+    {
+        float total_discount;
+        switch (quant_volumes)
+        {
+            case 2:
+                total_discount = 0.05;
+                break;
+            case 3:
+                total_discount = 0.10;
+                break;
+            case 4:
+                total_discount = 0.20;
+                break;
+            case 5:
+                total_discount = 0.25;
+                break;
+            default:
+                total_discount = 0;
+                break;
+        }
+
+    }
+    return total_cost;
 }
 
