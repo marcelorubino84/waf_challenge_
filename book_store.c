11c6de9aa6d086114e91017f90a1413c3deb4fe8
marcelorubino84 <marcelo.rubino@icloud.com>
2024-01-15 17:33:18 +0100

    In this second commit, I created a variable called cost_calculation which takes 2 parameter, the fisr one is an integer called quant_volumes, and the array volumes, also, located the prototype function at the top.

--- book_store.c
+++ book_store.c
@@ -2,8 +2,15 @@
 
 const int BOOK_PRICE = 8;
 
+float cost_calculation(int quant_volumes, int volumes[]);
+
 int main(void)
 {
     int volumes[] = {2, 2, 2, 1, 1};
+}
+
+float cost_calculation(int quant_volumes, int volumes[])
+{
+    float total_cost = 0.0;
 }
 
