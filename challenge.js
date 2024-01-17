dd2fb1eb06c5e863ec8d1ea024d4c794f4cdbd81
marcelorubino84 <marcelo.rubino@icloud.com>
2024-01-17 17:02:06 +0100

    Second commit: I created an object that keeps track of how many of each boo is in the cart. Then used a for each to count the number of each book in the shopping cart, and inside this, a count that checks if the book is in the cart, if it is not, it sets the count to 1, if it is, it will increment

--- challenge.js
+++ challenge.js
@@ -2,4 +2,13 @@ function cheapestOfferCalculator(cart)
 {
   const basePrice = 8;
   const discounts = [0, 0.05, 0.1, 0.2, 0.25];
+
+    const bookCounts = {};
+
+    cart.forEach((book) => {
+      bookCounts[book] = (bookCounts[book] || 0) + 1;
+    });
+
+
+    
 }
